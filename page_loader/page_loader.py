7e9d9a6bc2df972be029e41ee842ad9004cd3893
devriez <a.moiseikin@gmail.com>
2021-01-26 13:53:37 +0300

    make_file_path and test it

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -6,8 +6,10 @@ import os
 #     return
 
 def make_file_path(page_address, output_dir):
-    address_without_schema = page_address.split('//')
+    address_without_schema = page_address.split('//')[1]
+    print('address_without_schema', address_without_schema)
     splited_words_in_address = re.split(r'[^a-zA-Z0-9]', address_without_schema)
     file_name = '-'.join(splited_words_in_address) + '.html'
+    print('file_name', file_name)
     file_path = os.path.join(output_dir, file_name)
     return file_path
