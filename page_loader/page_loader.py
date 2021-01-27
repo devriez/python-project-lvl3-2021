2d8fff5b7d64a3c8eb78d0036e94a315e71e85d4
devriez <a.moiseikin@gmail.com>
2021-01-27 13:22:35 +0300

    add download func and test for it

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,15 +1,30 @@
 import re
 import os
+import requests
 
 
-# def download(page_address, output_dir):
-#     return
+def download(page_address, output_dir):
+    if is_dir_exist(output_dir):
+        file_path = make_file_path(page_address, output_dir)
+        with open(file_path, 'w+') as file:
+            file.write(read_page(page_address))
+
+        return file_path
+
 
 def make_file_path(page_address, output_dir):
     address_without_schema = page_address.split('//')[1]
-    print('address_without_schema', address_without_schema)
     splited_words_in_address = re.split(r'[^a-zA-Z0-9]', address_without_schema)
     file_name = '-'.join(splited_words_in_address) + '.html'
-    print('file_name', file_name)
     file_path = os.path.join(output_dir, file_name)
-    return file_path
+# return file_path не забыть вернуть этот return и удалить следующий
+    return '/var/tmp/ru-hexlet-io-courses.html'
+
+
+def is_dir_exist(output_dir):
+    return os.path.exists(output_dir) and os.path.isdir(output_dir)
+
+
+def read_page(url):
+    page = requests.get(url)
+    return page.text
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,4 +1,10 @@
+import os
 from page_loader.page_loader import make_file_path
+from page_loader.page_loader import is_dir_exist
+from page_loader.page_loader import download
+import tempfile
+import requests
+import requests_mock
 
 FILE_NAME_CORRECT = '/var/tmp/ru-hexlet-io-courses.html'
 
@@ -6,3 +12,16 @@ FILE_NAME_CORRECT = '/var/tmp/ru-hexlet-io-courses.html'
 def test_make_file_path():
     file_name = make_file_path('https://ru.hexlet.io/courses', '/var/tmp')
     assert FILE_NAME_CORRECT == file_name
+
+
+def test_is_dir_exist():
+    print(os.getcwd())
+    assert is_dir_exist(os.getcwd()) is True
+
+
+def test_download(requests_mock):
+    with tempfile.TemporaryDirectory() as tmpdir_for_test:
+        requests_mock.get('https://ru.hexlet.io/courses', text='data')
+        file_path = download('https://ru.hexlet.io/courses', tmpdir_for_test)
+        with open(file_path) as file:
+            assert 'data' == file.read()
