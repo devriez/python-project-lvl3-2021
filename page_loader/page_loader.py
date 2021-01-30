c15c6479f1630d784cd4433ed8b8508abf0ce086
devriez <a.moiseikin@gmail.com>
2021-01-30 18:02:49 +0300

    write all name and path funcs and tests

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,28 +1,59 @@
-import re
 import os
 import requests
+from urllib.parse import urlparse
+import re
 
 
-def download(page_address, output_dir):
-    if is_dir_exist(output_dir):
-        file_path = make_file_path(page_address, output_dir)
-        with open(file_path, 'w+') as file:
-            file.write(read_page(page_address))
-
-        return file_path
-
-
-def make_file_path(page_address, output_dir):
-    address_without_schema = page_address.split('//')[1]
-    splitted_address = re.split(r'[^a-zA-Z0-9]', address_without_schema)
-    page_name = '-'.join(splitted_address) + '.html'
-    file_path = os.path.join(output_dir, page_name)
-    return file_path
-#    return '/var/tmp/ru-hexlet-io-courses.html'
+# def download(page_address, output_dir):
+#     if is_dir_exist(output_dir):
+#         file_path = make_file_path(page_address, output_dir)
+#         file_html = read_page(page_address)
+#
+#         # with open(file_path, 'w+') as file:
+#         #     file.write(read_page(page_address))
+#
+#         return file_path
 
 
+# тест написан
+def make_page_file_name(page_address):
+    netloc = urlparse(page_address).netloc
+    path = urlparse(page_address).path
+    splitted_netloc = netloc.split('.')
+    netloc_kebab_case = '-'.join(splitted_netloc)
+    splitted_address = (netloc_kebab_case + path).split('/')
+    page_file_name = '-'.join(splitted_address) + '.html'
+    return page_file_name
+
+
+def make_path(name, root_dir):
+    path = os.path.join(root_dir, name)
+    return path
+
+
+# тест написан
+def make_name_dir_with_images(page_file_name):
+    path_without_ext, _ = os.path.splitext(page_file_name)
+    return path_without_ext + '_files'
+
+
+# тест написан
 def is_dir_exist(output_dir):
     return os.path.exists(output_dir) and os.path.isdir(output_dir)
+
+
+# тест написан
+def make_kebab_case_name(name):
+    if name[0] == '/':
+        name = name[1:]
+    splitted_name = re.split(r'[./]', name)
+    kebab_case_name = '-'.join(splitted_name)
+    return kebab_case_name
+
+
+def make_domain_kebab_case_name(url):
+    domain_name = urlparse(url).netloc
+    return make_kebab_case_name(domain_name)
 
 
 def read_page(url):
@@ -30,18 +61,16 @@ def read_page(url):
     return page.text
 
 
-def make_path_to_dir_with_images(file_path):
-    path_without_ext, _ = os.path.splitext(file_path)
-    return path_without_ext + '_files'
-
-
-def make_path_to_image(page_address, image_path):
-    address_without_schema = page_address.split('//')[1]
-    domain = address_without_schema.split('/')[0]
-    splitted_domain = re.split(r'[^a-zA-Z0-9]', domain)
+# test written
+def make_image_file_name(page_address, image_path):
+    domain_kebab_case = make_domain_kebab_case_name(page_address)
     image_path_without_ext, extension = os.path.splitext(image_path)
-    print('image_path_without_ext', image_path_without_ext)
-    splitted_image_path = re.split(r'[^a-zA-Z0-9]', image_path_without_ext)
-    print('splitted_image_path', splitted_image_path)
-    path_to_image = ('-'.join(splitted_domain) + '-'.join(splitted_image_path) + extension)
-    return path_to_image
+    image_path_kebab_case = make_kebab_case_name(image_path_without_ext)
+    image_name = domain_kebab_case + image_path_kebab_case + extension
+    return image_name
+
+
+# test written
+def make_image_url_absolut(page_url, image_url_relative):
+    domain_with_scheme = page_url - urlparse(page_url).path
+    return os.path.join(domain_with_scheme, image_url_relative)
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,34 +1,56 @@
 import os
-from page_loader.page_loader import make_file_path
+from page_loader.page_loader import make_page_file_name
 from page_loader.page_loader import is_dir_exist
 from page_loader.page_loader import download
-from page_loader.page_loader import make_path_to_image
+from page_loader.page_loader import make_image_file_name
+from page_loader.page_loader import make_name_dir_with_images
+from page_loader.page_loader import make_kebab_case_name
+from page_loader.page_loader import make_image_url_absolut
 import tempfile
 import requests_mock
 
-FILE_NAME_CORRECT = '/var/tmp/ru-hexlet-io-courses.html'
+
+def test_make_page_file_name():
+    PAGE_FILE_NAME_CORRECT = 'ru-hexlet-io-courses.html'
+    page_file_name = make_page_file_name('https://ru.hexlet.io/courses')
+    assert PAGE_FILE_NAME_CORRECT == page_file_name
 
 
-def test_make_file_path():
-    file_name = make_file_path('https://ru.hexlet.io/courses', '/var/tmp')
-    assert FILE_NAME_CORRECT == file_name
+def test_make_name_dir_with_images():
+    CORRECT_NAME_DIR_WITH_IMAGES = 'ru-hexlet-io-courses_files'
+    file_name = 'ru-hexlet-io-courses.html'
+    assert CORRECT_NAME_DIR_WITH_IMAGES == make_name_dir_with_images(file_name)
 
 
 def test_is_dir_exist():
     assert is_dir_exist(os.getcwd()) is True
 
 
-def test_download(requests_mock):
-    with tempfile.TemporaryDirectory() as tmpdir_for_test:
-        requests_mock.get('https://ru.hexlet.io/courses', text='data')
-        file_path = download('https://ru.hexlet.io/courses', tmpdir_for_test)
-        with open(file_path) as file:
-            assert 'data' == file.read()
+# def test_download(requests_mock):
+#     with tempfile.TemporaryDirectory() as tmpdir_for_test:
+#         requests_mock.get('https://ru.hexlet.io/courses', text='data')
+#         file_path = download('https://ru.hexlet.io/courses', tmpdir_for_test)
+#         with open(file_path) as file:
+#             assert 'data' == file.read()
 
 
-def test_make_path_to_imahe():
-    path_to_image = make_path_to_image(
+def test_make_kebab_case_name():
+    NAME = '/assets/professions/nodejs.pop'
+    CORRECT_KEBAB_CASE_NAME = 'assets-professions-nodejs-pop'
+    assert CORRECT_KEBAB_CASE_NAME == make_kebab_case_name(NAME)
+
+
+def test_make_image_file_name():
+    path_to_image = make_image_file_name(
         'https://ru.hexlet.io/courses', '/assets/professions/nodejs.png'
     )
     correct_path_to_image = 'ru-hexlet-io-assets-professions-nodejs.png'
     assert path_to_image == correct_path_to_image
+
+
+def test_make_image_url_absolut():
+    CORRECT_IMAGE_URL_ABSOLUTE = 'https://ru.hexlet.io/assets/professions/nodejs.png'
+    PAGE_URL = 'https://ru.hexlet.io/courses'
+    IMAGE_PATH = '/assets/professions/nodejs.png'
+    img_url_absolut = make_image_url_absolut(PAGE_URL, IMAGE_PATH)
+    assert CORRECT_IMAGE_URL_ABSOLUTE == img_url_absolut
